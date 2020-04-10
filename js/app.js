9365290e953d093994fdec0898da05b3477ef7cc
Laura-dn <56314608+Laura-dn@users.noreply.github.com>
2020-04-10 12:08:05 +0300

    upload

--- js/app.js
+++ js/app.js
@@ -1,3 +1,9 @@
+let DOMInputs = document.querySelectorAll("input");
+
+for(let i = 0; i < DOMInputs.length; i++) {
+    DOMInputs[i].addEventListener("keyup", preSearch);
+}
+
 function viewCars() {
     let DOMTable = document.querySelector("table");
     
@@ -9,12 +15,18 @@ function viewCars() {
         for(let j = 0; j < data.length; j++) {
             let DOMTd = document.createElement("td");
 
-            td.innerHTML = cars[i].data[j];
+            DOMTd.innerHTML = cars[i][data[j]];
             DOMTr.append(DOMTd);
         }
 
         DOMTable.append(DOMTr);
     }
+}
+
+function preSearch(ev) {
+    let DOMId = ev.target.id;
+
+    console.dir(ev);
 }
 
 viewCars();
